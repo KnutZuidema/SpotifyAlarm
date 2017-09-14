7667e5b361c05a8ed3cea4e1d8440917eb7c6af4
Knut Zuidema <knut.zuidema@gmail.com>
2017-09-14 20:33:20 +0200

    SearchAPI + SearchQueryBuilder

---
+++ src/win/knutzuidema/spotifyalarm/api/SearchAPI.java
@@ -0,0 +1,76 @@
+package win.knutzuidema.spotifyalarm.api;
+
+import org.apache.http.client.methods.HttpUriRequest;
+import org.json.JSONObject;
+import win.knutzuidema.spotifyalarm.datatypes.paging.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SearchAPI {
+
+    public Map<String, Paging> search(String query){
+        HttpUriRequest request = API
+                .requestBuilder("GET", "/search")
+                .addParameter("q", query)
+                .addParameter("type", "album,artist,track,playlist")
+                .build();
+
+        JSONObject json = API.getJSON(request);
+        Map<String, Paging> map = new LinkedHashMap<>();
+        if(!json.isNull("albums")) {
+            map.put("albums", new PagingAlbum(json.getJSONObject("albums")));
+        }
+        if(!json.isNull("artists")){
+            map.put("artists", new PagingArtist(json.getJSONObject("artists")));
+        }
+        if(!json.isNull("tracks")){
+            map.put("tracks", new PagingTrack(json.getJSONObject("tracks")));
+        }
+        if(!json.isNull("playlists")){
+            map.put("playlists", new PagingPlaylist(json.getJSONObject("playlists")));
+        }
+
+        return map;
+    }
+
+    public PagingAlbum searchAlbum(String query){
+        HttpUriRequest request = API
+                .requestBuilder("GET", "/search")
+                .addParameter("q", query)
+                .addParameter("type", "album")
+                .build();
+
+        return new PagingAlbum(API.getJSON(request).getJSONObject("albums"));
+    }
+
+    public PagingArtist searchArtist(String query){
+        HttpUriRequest request = API
+                .requestBuilder("GET", "/search")
+                .addParameter("q", query)
+                .addParameter("type", "artist")
+                .build();
+
+        return new PagingArtist(API.getJSON(request).getJSONObject("artist"));
+    }
+
+    public PagingTrack searchTrack(String query){
+        HttpUriRequest request = API
+                .requestBuilder("GET", "/search")
+                .addParameter("q", query)
+                .addParameter("type", "track")
+                .build();
+
+        return new PagingTrack(API.getJSON(request).getJSONObject("tracks"));
+    }
+
+    public PagingPlaylist searchPlaylist(String query){
+        HttpUriRequest request = API
+                .requestBuilder("GET", "/search")
+                .addParameter("q", query)
+                .addParameter("type", "playlist")
+                .build();
+
+        return new PagingPlaylist(API.getJSON(request).getJSONObject("playlists"));
+    }
+}
---
+++ src/win/knutzuidema/spotifyalarm/api/SearchQueryBuilder.java
@@ -0,0 +1,106 @@
+package win.knutzuidema.spotifyalarm.api;
+
+public class SearchQueryBuilder {
+    private static SearchQueryBuilder instance;
+    private StringBuilder keywords;
+    private StringBuilder exclusions;
+    private StringBuilder alternatives;
+    private String album;
+    private String artist;
+    private String track;
+    private String year;
+    private String genre;
+    private boolean isNew = false;
+    private boolean isHipster = false;
+
+    public static SearchQueryBuilder getInstance(){
+        if(instance == null){
+            instance = new SearchQueryBuilder();
+        }
+        return instance;
+    }
+
+    private SearchQueryBuilder(){
+        this.keywords = new StringBuilder();
+        this.exclusions = new StringBuilder();
+        this.alternatives = new StringBuilder();
+    }
+
+    public SearchQueryBuilder addKeywords(String keywords){
+        this.keywords.append(keywords.trim());
+        return this;
+    }
+
+    public SearchQueryBuilder addExclusions(String exclusions){
+        this.exclusions.append(" NOT ").append(exclusions.trim());
+        return this;
+    }
+
+    public SearchQueryBuilder addAlternatives(String alternatives){
+        this.alternatives.append(alternatives.trim());
+        return this;
+    }
+
+    public SearchQueryBuilder setAlbum(String album){
+        this.album = " album:" + album.trim();
+        return this;
+    }
+
+    public SearchQueryBuilder setArtist(String artist){
+        this.artist = " artist:" + artist.trim();
+        return this;
+    }
+
+    public SearchQueryBuilder setTrack(String track){
+        this.track = " track:" + track.trim();
+        return this;
+    }
+
+    public SearchQueryBuilder setYear(String year){
+        this.year = " year:" + year.trim();
+        return this;
+    }
+
+    public SearchQueryBuilder setYear(int year){
+        return setYear(String.valueOf(year));
+    }
+
+    public SearchQueryBuilder setYear(String from, String to){
+        this.year = " year:" + from.trim() + "-" + to.trim();
+        return this;
+    }
+
+    public SearchQueryBuilder setYear(int from, int to){
+        return setYear(String.valueOf(from), String.valueOf(to));
+    }
+
+    public SearchQueryBuilder setNew(boolean bool){
+        this.isNew = bool;
+        return this;
+    }
+
+    public SearchQueryBuilder setHipster(boolean bool){
+        this.isHipster = bool;
+        return this;
+    }
+
+    public SearchQueryBuilder setFieldGenre(String genre){
+        this.genre = "genre:" + genre.trim();
+        return this;
+    }
+
+    public String build(){
+
+        return keywords.toString() +
+                " OR " +
+                alternatives +
+                exclusions +
+                (album == null ? "" : album) +
+                (artist == null ? "" : artist) +
+                (track == null ? "" : track) +
+                (year == null ? "" : year) +
+                (genre == null ? "" : genre) +
+                (isHipster ? " tag:hipster" : "") +
+                (isNew ? " tag:new" : "");
+    }
+}
